d8a0bce1ba932e40980e1dc76f30de23d2415366
youssefssameh <x2ullahx@gmail.com>
2026-01-06 14:01:12 +0200

    upgrading yds_mrp to odoo18

---
+++ yds_mrp/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "YDS MRP Customizations",
+    "version": "18.0.1.0.0",
+    "author": "YDS Development Team",
+    "license": "AGPL-3",
+    "depends": ["mrp"],
+    "data": [
+        
+    ],
+    "installable": True,
+    "application": False,
+}
