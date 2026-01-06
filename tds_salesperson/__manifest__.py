18f1f4a2064059abd4a116c01d7790ee1acc525d
youssefssameh <x2ullahx@gmail.com>
2026-01-06 12:33:09 +0200

    upgrading tds_salesperson to odoo 18

---
+++ tds_salesperson/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'TDS Sales person',
+    'version': '18.0.1.0.0',
+    'author': 'TDS Dev Team',
+    'license': 'LGPL-3',
+    'depends': [
+        'base',
+        'sale'
+    ],
+    'data': [
+        'views/res_users.xml',
+        'views/res_partner.xml',
+    ],
+}
